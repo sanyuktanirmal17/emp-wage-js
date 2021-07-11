15fb261965067f21bf6cd18135afa37a489a4af4
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-07-12 00:22:20 +0530

    added: ability to add part time

--- Empwage.js
+++ Empwage.js
@@ -8,29 +8,24 @@ console.log("Welcome to EmployeeWage Computation Program")
 /**
  * Daily wage of employee
  */
-const IS_ABSENT = 0;
-const IS_FULL_TIME = 1;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
+ const IS_PART_TIME = 1;
+ const IS_FULL_TIME = 2;
+ const PART_TIME_HOURS = 4;
+ const FULL_TIME_HOURS = 8;
+ const WAGE_PER_HOUR = 20;
 
-let empCheck = Math.floor(Math.random() * 10);
-if(empCheck == IS_ABSENT){
-    console.log("Employee is ABSENT")
-} else{
-    console.log("Employee is PRESENT")
-} 
-
-let empWage = empCheck * WAGE_PER_HOUR;
-console.log("Employee Wage:" + empWage)
-/**
- * Check for employee 
- * 
- */
-const IS_ABSENT = 0;
-var empCheck = Math.floor(Math.random() * 10);
-if(empCheck == IS_ABSENT){
-    console.log("Employee is ABSENT")
-} else{
-    console.log("Employee is PRESENT")
-    
-}
+ let empHrs = 0;
+ /**
+  * Calculate emp wage for part time
+  */
+ let empCheck = Math.floor(Math.random() * 10) % 3;
+ if(empCheck == IS_FULL_TIME){
+     console.log("Employee is Full Time")
+ } if (empCheck == IS_PART_TIME) {
+     console.log("Employee is Part Time")
+ } else {
+     console.log("Employee is ABSENT")
+ }
+ 
+ let empWage = empCheck * WAGE_PER_HOUR;
+ console.log("Employee Wage:" + empWage)
