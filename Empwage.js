df817d927c1dddf9c6fb230e4593f61a087059c9
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-07-12 00:14:55 +0530

    added: ability to check daily wage

--- Empwage.js
+++ Empwage.js
@@ -6,6 +6,23 @@
 
 console.log("Welcome to EmployeeWage Computation Program")
 /**
+ * Daily wage of employee
+ */
+const IS_ABSENT = 0;
+const IS_FULL_TIME = 1;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empCheck = Math.floor(Math.random() * 10);
+if(empCheck == IS_ABSENT){
+    console.log("Employee is ABSENT")
+} else{
+    console.log("Employee is PRESENT")
+} 
+
+let empWage = empCheck * WAGE_PER_HOUR;
+console.log("Employee Wage:" + empWage)
+/**
  * Check for employee 
  * 
  */
