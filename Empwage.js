403bb15602d3a6a481b045a5dc34a74991b28cbe
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-07-11 23:56:35 +0530

    added: ability to check emp present or absent

--- Empwage.js
+++ Empwage.js
@@ -1,1 +1,18 @@
+/**
+ * Purpose: Employee Wage
+ * @author : Sanyukta Nirmal
+ * @since : 10/7/2021
+ */
+
 console.log("Welcome to EmployeeWage Computation Program")
+/**
+ * Check for employee 
+ */
+const IS_ABSENT = 0;
+var empCheck = Math.floor(Math.random() * 10);
+if(empCheck == IS_ABSENT){
+    console.log("Employee is ABSENT")
+} else{
+    console.log("Employee is PRESENT")
+    
+}
